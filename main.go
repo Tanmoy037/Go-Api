3851327f0be44a5957dec5305d46bbd35621f170
Tanmoy037 <tanmoysantra67@gmail.com>
2023-07-16 15:09:20 +0530

    data added

--- main.go
+++ main.go
@@ -6,10 +6,16 @@ import (
 )
 
 type book struct {
-	Id string `json:"id"`
+	ID string `json:"id"`
 	Title string `json:"title"`
 	Author string `json:"author"`
 	Quantity int `json:"quantity"`
+}
+
+var books = []book{     // saving data in memory not using database here
+	{ID: "1", Title: "In Search of Lost Time", Author: "Marcel Proust", Quantity: 2},
+	{ID: "2", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Quantity: 5},
+	{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 6},
 }
 
 
