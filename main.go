553a82bfa90e57b91beda5fb5e6fb9be91b37193
Tanmoy037 <tanmoysantra67@gmail.com>
2023-07-16 17:58:37 +0530

    PATCH request added

--- main.go
+++ main.go
@@ -56,7 +56,26 @@ func checkoutBook(c *gin.Context) {
 }
 
 func returnBook(c *gin.Context) {
+	id, ok := c.GetQuery("id")
 	
+	if !ok {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing id query parameter"})
+		return
+	}
+
+	book,err := getBookById(id)
+	if err!= nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+		return
+	}
+
+	if book.Quantity <= 0 {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Book not available"})
+		return
+	}
+	book.Quantity += 1
+	c.IndentedJSON(http.StatusOK, book)
+
 }
 
 func getBookById(id string) (*book, error) {
@@ -89,5 +108,6 @@ func main() {
 	router.GET("/books/:id", bookbyId)
 	router.POST("/books", createBook)
 	router.PATCH("/checkout", checkoutBook)
+	router.PATCH("/return", returnBook)
 	router.Run("localhost:8080")
 }
