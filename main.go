358053511c05a69936f01815bc44bbae80ab644e
Tanmoy037 <tanmoysantra67@gmail.com>
2023-07-16 15:11:03 +0530

    data added in slice

--- main.go
+++ main.go
@@ -12,11 +12,13 @@ type book struct {
 	Quantity int `json:"quantity"`
 }
 
-var books = []book{     // saving data in memory not using database here
+var books = []book{     // saving data in memory (slice) not using database here
 	{ID: "1", Title: "In Search of Lost Time", Author: "Marcel Proust", Quantity: 2},
 	{ID: "2", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Quantity: 5},
 	{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 6},
 }
+
+
 
 
 
